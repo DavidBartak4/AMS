c253e57c3cb5b0ead8c386bda5be0ea438850570
DavidBartak4 <david.bartak4@gmail.com>
2024-12-15 16:57:39 +0100

    DONE

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,11 +5,11 @@ import { AppService } from "./app.service"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const appService = app.get(AppService)
+  await appService.setupApp({url: "http://localhost:3000"})
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }))
   app.enableCors({ origin: "http://localhost:4000", methods: "*", allowedHeaders: "Content-Type, Authorization" })
   await app.listen(3000)
-  const appService = app.get(AppService)
-  await appService.setupApp({url: "http://localhost:3000"})
 }
 
 bootstrap()
